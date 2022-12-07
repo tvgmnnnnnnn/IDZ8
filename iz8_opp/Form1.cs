a660d5d6274c97bf167b36c8c0b4a785476ed94e
tvgmnnnnnnn <116604712+tvgmnnnnnnn@users.noreply.github.com>
2022-12-07 04:00:40 +0300

    Add files via upload

---
+++ iz8_opp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iz8_opp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox3.Text);
+            int n = int.Parse(textBox1.Text);
+            int r = int.Parse(textBox2.Text);
+            if (radioButton1.Checked)
+            {
+                textBox5.Text = Уравнение.Уравнение1(x, n, r).ToString();
+            }
+            else
+            {
+                textBox5.Text = Уравнение.Уравнение2(n, r).ToString();
+            }
+        }
+    }
+}
---
+++ iz8_opp/Уравнение.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iz8_opp
+{
+    internal class Уравнение
+    {
+        public static double Уравнение1(double x, int n, int r)
+        {
+            double result = 0;
+            double chisl = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                double znam=2;
+                if (i != 1)
+                {
+                    znam = znam * (i + 2);
+                }
+                if (i % 2 == 0)
+                    chisl = chisl + (i*Math.Pow(x, (i)));
+                else
+                    chisl = chisl - (i*Math.Pow(x, (i)));
+                result += chisl / znam;
+                chisl = 0;
+            }
+            return result;
+        }
+
+        public static double Уравнение2(int n, int r)
+        {
+            double result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; i <= r; i++)
+                {
+                    result += ((i+1)*(i+1)) / (i*i*i+j*j*j);
+                }
+            }
+            return result;
+        }
+    }
+}
